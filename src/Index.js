7985cff228063e44ea97c4475a14491b8864cc37
Joseph Chambers <joseph@michael-chambers.com>
2019-01-03 12:37:50 -0800

    organize src dir

--- src/Index.js
+++ src/Index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { Router } from '@reach/router'
 
-import Main from './Main'
-import JobsList from './JobsList'
+import Main from './container/Main'
+import JobsList from './jobs/List'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
