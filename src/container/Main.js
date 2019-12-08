4896a604c6d148cc2a03ec7987592ae223d442d1
Joseph Chambers <joseph@michael-chambers.com>
2019-12-08 11:57:22 -0700

    - updates deps - use hooks and effects

--- src/container/Main.js
+++ src/container/Main.js
@@ -2,17 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Menu from '../components/Menu';
 
-const Main = props => {
-  const { children } = props;
+const Main = ({ children }) => (
+  <>
+    <Menu />
 
-  return (
-    <>
-      <Menu />
-
-      <div className="container mx-auto px-4">{children}</div>
-    </>
-  );
-};
+    <div className="container mx-auto px-4">{children}</div>
+  </>
+);
 
 Main.propTypes = {
   children: PropTypes.element.isRequired,
--- src/jobs/List.js
+++ src/jobs/List.js
@@ -1,48 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import uuid from 'uuid';
 import API from '../../utils/API';
 import Job from '../components/Job';
 
-export default class JobsList extends Component {
-  state = {
-    jobs: [],
+const JobsList = () => {
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    const forHire = await API.get('/forhire.json');
+
+    const fetchedJobs = forHire.data.data.children
+      .map(listing => listing.data)
+      .filter(word => word.title.toLowerCase().includes('hiring'));
+
+    setJobs(fetchedJobs);
   };
 
-  async componentDidMount() {
-    await this.fetchJobs();
-  }
+  useEffect(() => {
+    fetchJobs();
+  }, []);
 
-  async fetchJobs() {
-    try {
-      const forHire = await API.get('/forhire.json');
+  return (
+    <div className="flex flex-wrap justify-between sm:w-full md:w-full">
+      {jobs.map(job => (
+        <Job author={job.author} key={uuid()} title={job.title} url={job.url} />
+      ))}
+    </div>
+  );
+};
 
-      const jobs = forHire.data.data.children
-        .map(listing => listing.data)
-        .filter(word => word.title.toLowerCase().includes('hiring'));
-
-      this.setState({
-        jobs,
-      });
-    } catch (e) {
-      // handle error, for real later.
-      console.log(e);
-    }
-  }
-
-  render() {
-    const { jobs } = this.state;
-
-    return (
-      <div className="flex flex-wrap justify-between sm:w-full md:w-full">
-        {jobs.map(job => (
-          <Job
-            author={job.author}
-            key={uuid()}
-            title={job.title}
-            url={job.url}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+export default JobsList;
